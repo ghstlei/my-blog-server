4fa11e68b98ebf58c1e4afe7e93cd0574387f494
Cibelly Souza <cibellyneuza.silvaesouza@thomsonreuters.com>
2024-04-05 16:00:49 -0300

    add upvote endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 
+let articlesInfo = [
+  { name: "learn-react", upvotes: 0 },
+  { name: "learn-node", upvotes: 0 },
+  { name: "learn-mongodb", upvotes: 0 },
+];
+
 const app = express();
 app.use(express.json());
 
@@ -12,6 +18,19 @@ app.get("/hello/:name/goodbye/:deadname", (req, res) => {
   const { name, deadname } = req.params;
   res.send(`Hello ${name}! Goodbye ${deadname}!`);
 }); */
+
+app.put("/api/articles/:name/upvote"),
+  (req, res) => {
+    const { name } = req.params;
+    const articles = articlesInfo.find((article) => article.name === name);
+
+    if (articles) {
+      articles.upvotes += 1;
+      res.send(`${name} now has ${articles.upvotes} upvotes.`);
+    } else {
+      res.send("Article not found.");
+    }
+  };
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
