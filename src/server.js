f5d3ff676a998f11c58a3640c9e35cff6a78fe9e
Cibelly Souza <cibellyneuza.silvaesouza@thomsonreuters.com>
2024-04-11 15:45:17 -0300

    mongo db data input and output

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 import express from "express";
-
-let articlesInfo = [
-  { name: "learn-react", upvotes: 0 },
-  { name: "learn-node", upvotes: 0 },
-  { name: "learn-mongodb", upvotes: 0 },
-];
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 
-/*app.post("/hello", (req, res) => {
+app.get("/api/articles/:name", async (req, res) => {
+  console.log(req.params);
+  const { name } = req.params;
+
+  const client = new MongoClient("mongodb://localhost:27017/");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else {
+    res.sendStatus(404), send("Article Not Found");
+  }
+});
+
+app.post("/hello", (req, res) => {
   res.send(`Hello ${req.body.name}!`);
 });
 
@@ -17,20 +29,41 @@ app.get("/hello/:name/goodbye/:deadname", (req, res) => {
   console.log(req.params);
   const { name, deadname } = req.params;
   res.send(`Hello ${name}! Goodbye ${deadname}!`);
-}); */
+});
 
-app.put("/api/articles/:name/upvote"),
-  (req, res) => {
-    const { name } = req.params;
-    const articles = articlesInfo.find((article) => article.name === name);
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
 
-    if (articles) {
-      articles.upvotes += 1;
-      res.send(`${name} now has ${articles.upvotes} upvotes.`);
-    } else {
-      res.send("Article not found.");
-    }
-  };
+  if (article) {
+    article.upvotes += 1;
+    res.send(`${name} article now has ${article.upvotes} upvotes.`);
+  } else {
+    res.send("Article not found.");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  if (!postedBy || !text) {
+    return res
+      .status(400)
+      .send("Both 'postedBy' and 'text' fields are required.");
+  }
+
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    console.log("Article:", article);
+    console.log("Article comments:", article.comments);
+    res.send(article.comments);
+  } else {
+    res.send("Article not found.");
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
